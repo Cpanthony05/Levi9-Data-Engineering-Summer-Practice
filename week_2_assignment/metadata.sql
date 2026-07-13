71303b44bc6c39b33fbb5b3d78e1fb021815e1de
Cpanthony05 <c.paul.anthony05@gmail.com>
2026-07-13 19:25:51 +0300

    week_2_done

---
+++ week_2_assignment/metadata.sql
@@ -0,0 +1,29 @@
+-- 10.1
+-- Column name : CNP
+-- Romanian personal numeric code (unique identifier for a person). used for verification
+-- Data type : CHAR(13)
+-- Sensitive data : yes
+
+-- 10.2
+-- Column name : Amount
+-- Value of a payment made by a student
+-- Data type : Decimal(10,2)
+-- Sensitive : no
+
+-- 10.3
+-- Column name : Score
+-- Rating given by an instructor; it reflects student performance
+-- Data type : SMALLINT
+-- Sensitive : no
+
+-- 10.4
+-- Column name : Email
+-- Email for a student
+-- Data type : VARCHAR(100)
+-- Sensitive : yes
+
+-- 10.5
+-- Column name : Status
+-- Current state of a student's enrollment
+-- Data type : VARCHAR(20)
+-- Sensitive : No
