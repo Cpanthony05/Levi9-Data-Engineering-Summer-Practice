3f1fa19b128a7876486d2986c122480c6dd49592
Cpanthony05 <c.paul.anthony05@gmail.com>
2026-08-05 11:49:42 +0300

    trips fact table

---
+++ project/dbt_project/models/marts/fct_trips.sql
@@ -0,0 +1,31 @@
+-- models/marts/fct_trips.sql
+{{
+    config(
+        materialized='incremental',
+        unique_key='ride_id',
+        incremental_strategy='merge'
+    )
+}}
+
+select
+    t.ride_id,
+    t.rideable_type,
+    t.started_at,
+    t.ended_at,
+    t.trip_duration_seconds,
+    t.member_casual,
+    ss.station_id as start_station_id,
+    ss.station_name as start_station_name,
+    es.station_id as end_station_id,
+    es.station_name as end_station_name
+from {{ ref('stg_trips') }} t
+left join {{ ref('dim_station_snapshot') }} ss
+    on t.start_station_id = ss.station_id
+    and (t.started_at < ss.dbt_valid_to or ss.dbt_valid_to is null)
+left join {{ ref('dim_station_snapshot') }} es
+    on t.end_station_id = es.station_id
+    and (t.ended_at < es.dbt_valid_to or es.dbt_valid_to is null)
+
+{% if is_incremental() %}
+where t.started_at > (select max(started_at) from {{ this }})
+{% endif %}
